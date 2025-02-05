0aef1bf959c48f2b542ca40fa80c30538d583a4f
giusmili <giusmili67@gmail.com>
2025-02-05 14:56:23 +0100

    js

---
+++ js/app.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", e => {
+        // un écouteur js avec l'objet document
+        console.log(e.target);
+
+        /* prompt */
+        let firstName, lastName, age, tabUser, element, btn, stock, unstock, userIndex, image;
+
+        image = document.querySelector("img");
+        btn = document.querySelector("section button"); /* btn pour l'utilisateur */
+
+        element = document.querySelector("section h2");
+
+        /* btn event */
+
+        btn.addEventListener("click", () => {
+                // alert(btn.tagName)
+                callPrompt.dialog();
+        });
+
+        const callPrompt = {
+
+                logo: "https://static.vecteezy.com/system/resources/previews/027/127/463/non_2x/javascript-logo-javascript-icon-transparent-free-png.png",
+
+                // méthode prompt
+                dialog() {
+                        firstName = String(window.prompt("Ajoutez votre nom", ""));
+                        lastName = String(window.prompt("Ajoutez votre prénom", ""));
+                        age = Number(window.prompt("Ajoutez votre age", ""));
+
+                        if (!firstName || !lastName || !age) {
+                                alert("tous les champs obligatoire");
+                                // location.reload()
+                                element.innerHTML = `<span class="warning">Tous les champs obligatoire!!!</span>`;
+                        } else {
+                                tabUser = [];
+
+                                tabUser.push(firstName, lastName, age);
+
+                                console.info(tabUser);
+
+                                /* zone de stockage */
+
+                                stock = localStorage.setItem("user", JSON.stringify(tabUser)); /* item + valeur */
+
+                                unstock = localStorage.getItem("user");
+                                userIndex = JSON.parse(unstock).join(" / ");
+                                console.log(userIndex);
+
+                                element.innerHTML = `🚀 Bonjour <strong>${lastName}</strong> voici vos données ${userIndex}`;
+                        }
+                }
+        };
+
+        image.src = callPrompt.logo;
+});
